d2acfea0e2440f71b911940d268f1ba7ebbadcf2
ANIL AYAYDIN <anil.ayaydin@gmail.com>
2022-09-06 15:40:44 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace RotateAnArray
+{
+    class Program
+    {
+        
+        static void RotateArrayLeft(int[] input) {
+            
+            int temp = input[0];
+
+            for(int i = 0; i < input.Length - 1; i++) {
+                
+                input[i] = input[i+1];
+            
+            }
+
+            input[input.Length - 1] = temp;
+
+        }
+
+        static void RotateArrayRight(int[]input) {
+            int temp = input[input.Length - 1];
+
+            for(int i = input.Length - 1; i > 0; i--) {
+                input[i] = input[i - 1];
+            }
+
+            input[0] = temp;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] arr = {1,2,3,4,5,6};
+            RotateArrayLeft(arr);
+            Array.ForEach(arr, Console.WriteLine);
+
+            RotateArrayRight(arr);
+            Array.ForEach(arr, Console.WriteLine);
+        }
+    }
+}
